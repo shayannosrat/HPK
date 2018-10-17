c330b33f7043c4211c9e00c59bd4cf436b8abe55
Till Kobbe <Till@tills-air>
2018-10-17 10:26:17 +0200

    Javadoc added

--- hpk/src/main/java/de/lab4inf/wrb/WRBScript.java
+++ hpk/src/main/java/de/lab4inf/wrb/WRBScript.java
@@ -18,6 +18,12 @@ public class WRBScript implements Script {
 	
 	public WRBScript() {}
 	
+	/**
+     * Parse the given String into an internal representation (e.g. as AST) and
+     * evaluate the expression(s).
+     * @param definition of the script expression(s) as a String
+     * @return the result of the parsed evaluation
+     */
 	public double parse(String definition) throws IllegalArgumentException{
 		CharStream inputStream = CharStreams.fromString(definition+'\n');
 		WRBLexer lexer = new WRBLexer(inputStream);
@@ -27,7 +33,7 @@ public class WRBScript implements Script {
 		
 		try {
 			r = parser.run(); // Token-Stream parsen
-			lastResult = Double.valueOf(ob.visitRun(r));// Ergebnis ausgeben
+			lastResult = Double.valueOf(ob.visitRun(r)); // Ergebnis ausgeben
 			
 		} catch(Exception e) {
 			throw new IllegalArgumentException("Ungültige Eingabe");
@@ -35,6 +41,13 @@ public class WRBScript implements Script {
 		return lastResult;
 	}
 	
+	/**
+     * Parse the InputStream into an internal representation (e.g. as AST) and
+     * evaluate the expression(s).
+     * @param defStream of the script expression(s) as InputStream
+     * @return the result of the parsed evaluation
+     * @throws IOException in case of a stream I/O error
+     */
 	public double parse(InputStream defStream) throws IOException {
 		CharStream inputStream = CharStreams.fromStream(defStream);
 		WRBLexer lexer = new WRBLexer(inputStream);
@@ -44,7 +57,7 @@ public class WRBScript implements Script {
 		
 		try {
 			r = parser.run(); // Token-Stream parsen
-			lastResult = Double.valueOf(ob.visitRun(r));// Ergebnis ausgeben
+			lastResult = Double.valueOf(ob.visitRun(r)); // Ergebnis ausgeben
 			
 		} catch(Exception e) {
 			throw new IllegalArgumentException("Ungültige Eingabe");
@@ -52,26 +65,53 @@ public class WRBScript implements Script {
 		return lastResult;
 	}
 	
+	/**
+     * Get all script known variable names.
+     * @return set with the variables names
+     */
 	public Set<String> getVariableNames() {
 		return new HashSet<String>();
 	}
 		
-	
+	/**
+     * Get the value of a (unique!) named variable.
+     * @param name of the variable
+     * @return the actual variable value
+     */
 	public double getVariable(String name) {
 		return parse(name);
 	}
 	
+	/**
+     * Set the value of a named variable.
+     * @param name of the variable to be unique
+     * @param value the new variable setting
+     */
 	public void setVariable(String name, double value) {
 		String expr = name+'='+Double.toString(value);
 		parse(expr);
 	}
 	
+	/**
+     * Add a named function to this script.
+     * @param name of the function to be unique
+     * @param fct to add 
+     */
 	public void setFunction(String name, Function fct) {}
 	
+	/**
+     * Get all script known function names.
+     * @return set with the function names
+     */
 	public Set<String> getFunctionNames() {
 		return new HashSet<String>();
 	}
 	
+	/**
+     * Get a (unique!) named function.
+     * @param name of the function
+     * @return an implementation
+     */
 	public Function getFunction(String name) {
 		return null;
 	}
